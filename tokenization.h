e1b41be55f641fa920e76b8210baa8750ac88536
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-01-12 20:17:51 +0900

    wait

--- tokenization.h
+++ tokenization.h
@@ -3,6 +3,17 @@
 #ifndef _TOKENIZATION_H
 # define _TOKENIZATION_H
 
+typedef struct {char v[13]} L13str;
+typedef struct {char v[79]} L79str;
+typedef union {
+    L13str index[6];
+    L79str const_value;
+} keywords;
+
+const L79str = {"
+
+int is(L13str* v, L13str)
+
 void tokenization(char* target, char* ret, int length) {
     do {
         //pass
