23956b041f27bc6e81aecd93a33636c788029af6
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-01-12 20:02:38 +0900

    umm

---
+++ tokenization.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef _TOKENIZATION_H
+# define _TOKENIZATION_H
+
+void tokenization(char* target, char* ret, int length) {
+    do {
+        //pass
+    } while (++target);
+}
+
+#endif
